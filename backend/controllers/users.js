83950b1746ffd418e5b84597069e467593e61a7b
Timna <timnar312@gmail.com>
2022-01-08 15:59:22 +0200

    error message login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
     .catch((err) => {
       // authentication error
       if (err.name === "Error") {
-        return res.status(401).send({ message: err.name });
+        return res.status(401).send({ message: err.message });
       }
       next(err);
     });
